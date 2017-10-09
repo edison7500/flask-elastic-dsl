503227cddbee97a505777ecf61617dabd725bc43
jiaxin <edison7500@gmail.com>
2017-10-09 16:11:08 +0800

    update

--- flask_elastic_dsl.py
+++ flask_elastic_dsl.py
@@ -13,7 +13,7 @@ class ElasticDSL(object):
     def init_app(self, app):
         self.elastic_urls = list()
 
-        _elastic_host = app.config.get("ELASTICSEARCH_HOST", "localhost:9200")
+        _elastic_host = app.config.get("ELASTIC_HOST", "localhost:9200")
         _elastic_user = app.config.get("ELASTIC_USER", None)
         _elastic_passwd = app.config.get("ELASTIC_PASSWORD", None)
 
